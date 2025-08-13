1c00790100955db600b6d81e4dfcb9807783d1ae
SaulPuga <ve17691@innovaccion.mx>
2025-08-12 22:41:47 -0600

    agregando bd h2

---
+++ src/test/java/com/examen/microservicios/mclient/MclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.microservicios.mclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
